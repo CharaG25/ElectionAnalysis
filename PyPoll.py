065247e6ca282e063ecb5b067fbff5b1aa221b76
CharaG25 <95732113+CharaG25@users.noreply.github.com>
2022-02-05 11:30:49 -0600

    Add files via upload

---
+++ PyPoll.py
@@ -0,0 +1,5 @@
+#The data we need to retrieve
+#A complete list of candidates who recieved votes
+#The percentage of votes each candidate won
+#The total number of votes each candidate won
+#The winner of the election based on popular vote
